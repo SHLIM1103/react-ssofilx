ee40131de029bf0f0f5b685f65880ac0f4346445
shlim1103 <gusl5525@gmail.com>
2021-05-25 19:16:17 +0900

    react-helmet

--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -1,6 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import styled from "styled-components"
+import Helmet from "react-helmet"
 import Loader from "Components/Loader"
 import Section from "Components/Section"
 import Message from "Components/Message"
@@ -10,59 +11,69 @@ const Container = styled.div`
   padding: 20px;
 `
 
-const TVPresenter = ({ topRated, popular, airingToday, loading, error }) =>
-  loading ? (
-    <Loader />
-  ) : (
-    <Container>
-      {topRated && topRated.length > 0 && (
-        <Section title="Top Rated Shows">
-          {topRated.map((show) => (
-            <Poster
-              key={show.id}
-              id={show.id}
-              imageUrl={show.poster_path}
-              title={show.original_name}
-              rating={show.vote_average}
-              year={show.first_air_date && show.first_air_date.substring(0, 4)}
-              isshow={true}
-            />
-          ))}
-        </Section>
-      )}
-      {popular && popular.length > 0 && (
-        <Section title="Popular Shows">
-          {popular.map((show) => (
-            <Poster
-              key={show.id}
-              id={show.id}
-              imageUrl={show.poster_path}
-              title={show.original_name}
-              rating={show.vote_average}
-              year={show.first_air_date.substring(0, 4)}
-              isshow={true}
-            />
-          ))}
-        </Section>
-      )}
-      {airingToday && airingToday.length > 0 && (
-        <Section title="Airing Today">
-          {airingToday.map((show) => (
-            <Poster
-              key={show.id}
-              id={show.id}
-              imageUrl={show.poster_path}
-              title={show.original_name}
-              rating={show.vote_average}
-              year={show.first_air_date && show.first_air_date.substring(0, 4)}
-              isshow={true}
-            />
-          ))}
-        </Section>
-      )}
-      {error && <Message color="#d63031" text={error} />}
-    </Container>
-  )
+const TVPresenter = ({ topRated, popular, airingToday, loading, error }) => (
+  <>
+    <Helmet>
+      <title>TV Shows | SSoflix</title>
+    </Helmet>
+    {loading ? (
+      <Loader />
+    ) : (
+      <Container>
+        {topRated && topRated.length > 0 && (
+          <Section title="Top Rated Shows">
+            {topRated.map((show) => (
+              <Poster
+                key={show.id}
+                id={show.id}
+                imageUrl={show.poster_path}
+                title={show.original_name}
+                rating={show.vote_average}
+                year={
+                  show.first_air_date && show.first_air_date.substring(0, 4)
+                }
+                isshow={true}
+              />
+            ))}
+          </Section>
+        )}
+        {popular && popular.length > 0 && (
+          <Section title="Popular Shows">
+            {popular.map((show) => (
+              <Poster
+                key={show.id}
+                id={show.id}
+                imageUrl={show.poster_path}
+                title={show.original_name}
+                rating={show.vote_average}
+                year={show.first_air_date.substring(0, 4)}
+                isshow={true}
+              />
+            ))}
+          </Section>
+        )}
+        {airingToday && airingToday.length > 0 && (
+          <Section title="Airing Today">
+            {airingToday.map((show) => (
+              <Poster
+                key={show.id}
+                id={show.id}
+                imageUrl={show.poster_path}
+                title={show.original_name}
+                rating={show.vote_average}
+                year={
+                  show.first_air_date && show.first_air_date.substring(0, 4)
+                }
+                isshow={true}
+              />
+            ))}
+          </Section>
+        )}
+        {error && <Message color="#d63031" text={error} />}
+      </Container>
+    )}
+  </>
+)
 
 TVPresenter.propTypes = {
   topRated: PropTypes.array,
